dc53c0ead7b49a5e22e61098672a39f8ed39ece7
Purag Moumdjian <me@purag.me>
2018-12-07 20:58:15 -0800

    core: set up dynamic color scheme changing for linux and windows

--- meat/core/p13n/filters.py
+++ meat/core/p13n/filters.py
@@ -7,3 +7,7 @@ def to_hex (i):
 def to_win_color (hex):
   hex = nohash(hex)
   return '00' + hex[4:6] + hex[2:4] + hex[0:2]
+
+def to_gnome_color (hex):
+  hex = nohash(hex)
+  return '#' + hex[0:2]*2 + hex[2:4]*2 + hex[4:6]*2
---
+++ meat/core/p13n/templates/gnome.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+profile=$(gsettings get org.gnome.Terminal.ProfilesList list | tr -d \'[])
+dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/palette" "[
+  '{{ colors.black.normal | to_gnome_color }}',
+  '{{ colors.red.normal | to_gnome_color }}',
+  '{{ colors.green.normal | to_gnome_color }}',
+  '{{ colors.yellow.normal | to_gnome_color }}',
+  '{{ colors.blue.normal | to_gnome_color }}',
+  '{{ colors.violet.normal | to_gnome_color }}',
+  '{{ colors.orange.normal | to_gnome_color }}',
+  '{{ colors.white.normal | to_gnome_color }}',
+  '{{ colors.black.bold | to_gnome_color }}',
+  '{{ colors.red.bold | to_gnome_color }}',
+  '{{ colors.green.bold | to_gnome_color }}',
+  '{{ colors.yellow.bold | to_gnome_color }}',
+  '{{ colors.blue.bold | to_gnome_color }}',
+  '{{ colors.violet.bold | to_gnome_color }}',
+  '{{ colors.orange.bold | to_gnome_color }}',
+  '{{ colors.white.bold | to_gnome_color }}'
+]"
+dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/background-color" "'{{ colors.bg.normal | to_gnome_color }}'"
+dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/highlight-background-color" "'{{ colors.bg.bold | to_gnome_color }}'"
+dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/foreground-color" "'{{ colors.fg.normal | to_gnome_color }}'"
+dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/font" "'{{ font.linux.name }} {{ font.linux.size }}'"
+
+export __red="$(tput setaf {{ colors.codes.red }})"
+export __green="$(tput setaf {{ colors.codes.green }})"
+export __yellow="$(tput setaf {{ colors.codes.yellow }})"
+export __blue="$(tput setaf {{ colors.codes.blue }})"
+export __magenta="$(tput setaf {{ colors.codes.magenta }})"
+export __orange="$(tput setaf {{ colors.codes.orange }})"
+export __gray="$(tput setaf {{ colors.codes.gray }})"
