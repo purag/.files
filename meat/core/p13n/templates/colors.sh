14602b838057f63259410d2b1fe76bb3e5100720
Purag Moumdjian <me@purag.me>
2018-12-22 14:36:28 -0800

    core: set colors separately. this makes schemes work in mac and windows

---
+++ meat/core/p13n/templates/colors.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export __red="$(tput setaf {{ colors.codes.red }})"
+export __green="$(tput setaf {{ colors.codes.green }})"
+export __yellow="$(tput setaf {{ colors.codes.yellow }})"
+export __blue="$(tput setaf {{ colors.codes.blue }})"
+export __magenta="$(tput setaf {{ colors.codes.magenta }})"
+export __orange="$(tput setaf {{ colors.codes.orange }})"
+export __gray="$(tput setaf {{ colors.codes.gray }})"
+export __cyan="$(tput setaf {{ colors.codes.cyan }})"
--- meat/core/p13n/templates/gnome.sh
+++ meat/core/p13n/templates/gnome.sh
@@ -23,12 +23,3 @@ dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/background-color" "'
 dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/highlight-background-color" "'{{ colors.bg.bold | to_gnome_color }}'"
 dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/foreground-color" "'{{ colors.fg.normal | to_gnome_color }}'"
 dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/font" "'{{ font.linux.name }} {{ font.linux.size }}'"
-
-export __red="$(tput setaf {{ colors.codes.red }})"
-export __green="$(tput setaf {{ colors.codes.green }})"
-export __yellow="$(tput setaf {{ colors.codes.yellow }})"
-export __blue="$(tput setaf {{ colors.codes.blue }})"
-export __magenta="$(tput setaf {{ colors.codes.magenta }})"
-export __orange="$(tput setaf {{ colors.codes.orange }})"
-export __gray="$(tput setaf {{ colors.codes.gray }})"
-export __cyan="$(tput setaf {{ colors.codes.cyan }})"
