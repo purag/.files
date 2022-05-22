fe67c7db1b98ec33bc3891daf1b72d9300c9bc13
Purag Moumdjian <me@purag.me>
2022-05-22 10:47:32 -0700

    p13n: whoops, use up-to-date zenbu output

--- meat/core/p13n/bin/colors.sh
+++ meat/core/p13n/bin/colors.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+export __black="$(tput setaf 0)"
 export __red="$(tput setaf 1)"
 export __green="$(tput setaf 2)"
 export __yellow="$(tput setaf 3)"
 export __blue="$(tput setaf 4)"
-export __magenta="$(tput setaf 5)"
-export __orange="$(tput setaf 11)"
-export __gray="$(tput setaf 8)"
+export __violet="$(tput setaf 5)"
 export __cyan="$(tput setaf 6)"
+export __white="$(tput setaf 7)"
+
+export __bright_black="$(tput setaf 8)"
+export __bright_red="$(tput setaf 9)"
+export __bright_green="$(tput setaf 10)"
+export __bright_yellow="$(tput setaf 11)"
+export __bright_blue="$(tput setaf 12)"
+export __bright_violet="$(tput setaf 13)"
+export __bright_cyan="$(tput setaf 14)"
+export __bright_white="$(tput setaf 15)"
+
+# Aliases
+export __orange=$__bright_yellow
+export __dark_gray=$__bright_black
+export __gray=$__bright_white
--- meat/core/p13n/bin/gnome.sh
+++ meat/core/p13n/bin/gnome.sh
@@ -2,24 +2,24 @@
 
 profile=$(gsettings get org.gnome.Terminal.ProfilesList list | tr -d \'[])
 dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/palette" "[
-  '#1A1A25252C2C',
-  '#E3E31E1E2626',
-  '#8181C1C14949',
-  '#FCFCB1B13333',
-  '#4F4F8D8DCACA',
-  '#EFEF3B3B5F5F',
-  '#2525B9B9F0F0',
-  '#FFFFFFFFFFFF',
-  '#3A3A43434A4A',
-  '#A2A21D1D2121',
-  '#515194944545',
-  '#F0F05E5E2525',
-  '#12126767ADAD',
-  '#BBBB26264444',
-  '#25255353F0F0',
-  '#AFAFBEBEC7C7'
+  '#222231313A3A',
+  '#EBEB36363131',
+  '#9191C8C85A5A',
+  '#FDFDBDBD4141',
+  '#5F5F9F9FD4D4',
+  '#F4F454547171',
+  '#2424C5C5F3F3',
+  '#FEFEFFFFFFFF',
+  '#4A4A54545C5C',
+  '#B3B32E2E2b2b',
+  '#6161A2A25656',
+  '#F5F574742F2F',
+  '#0D0D7C7CBBBB',
+  '#C9C93B3B5555',
+  '#2F2F6D6DF3F3',
+  '#BCBCC9C9D1D1'
 ]"
-dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/background-color" "'#1A1A25252C2C'"
-dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/highlight-background-color" "'#3A3A43434A4A'"
-dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/foreground-color" "'#AFAFBEBEC7C7'"
-dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/font" "'Fira Mono Medium 11'"
+dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/background-color" "'#222231313A3A'"
+dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/highlight-background-color" "'#4A4A54545C5C'"
+dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/foreground-color" "'#BCBCC9C9D1D1'"
+dconf write "/org/gnome/terminal/legacy/profiles:/:$profile/font" "'Fira Code Retina 11'"
